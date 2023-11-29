d8504ff3e8c14f19f526de0f1119fe228e0b995a
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-11-29 17:06:17 +0100

    zad8

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Images from "./components/Images/Images";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Images />
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Images/Images.jsx
@@ -0,0 +1,7 @@
+import './Images.css'
+
+const Images = () => {
+return (<div className={'Images'}>Sample text from Images component</div>)
+}
+
+export default Images;
