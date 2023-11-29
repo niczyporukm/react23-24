2061b27941a9deb69b96ba0c69721fa9ca737cf7
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-11-29 17:05:21 +0100

    zad7

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import MyFirstComponent from "./components/MyFirstComponent/MyFirstComponent.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <MyFirstComponent />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
