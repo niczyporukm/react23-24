ecf838cadfc25360e435fe18417b258b76930900
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-11-29 17:09:25 +0100

    zad9

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import Images from "./components/Images/Images";
 
@@ -10,14 +10,6 @@ function App() {
   return (
     <>
       <Images />
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -1,7 +1,18 @@
-import './Images.css'
+import "./Images.css";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "../../../public/vite.svg";
 
 const Images = () => {
-return (<div className={'Images'}>Sample text from Images component</div>)
-}
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+};
 
 export default Images;
