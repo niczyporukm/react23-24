1cf38178c66674494929515cc297128245d19e88
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-12-16 20:52:38 +0100

    ps3-zad-6

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -4,10 +4,19 @@ import "./UserForm.css";
 const UserForm = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [userAge, setUserAge] = useState();
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
+  };
+
+  const handleSetUserAge = (event) => {
+    const { value } = event.target;
+    console.log(value);
+    if (value.length === 0 || (value > 0 && value < 99)) {
+      setUserAge(value);
+    }
   };
 
   return (
@@ -28,6 +37,15 @@ const UserForm = () => {
       <button type="button" onClick={togglePasswordVisibility}>
         {showPassword ? "Hide Password" : "Show Password"}
       </button>
+      <label>Age</label>
+      <input
+        type="number"
+        placeholder="Age"
+        min={0}
+        max={50}
+        onChange={handleSetUserAge}
+        value={userAge}
+      />
     </form>
   );
 };
