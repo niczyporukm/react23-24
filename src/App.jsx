4730027ba22c7dd0d70f7a6ca45af8e7dec07ba6
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-12-16 19:50:18 +0100

    ps3-zad3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import Paragraph from "./components/Paragraph/Paragraph";
 import CounterButton from "./components/CounterButton/CounterButton";
+import Button from "./components/Button/Button";
+import { useState } from "react";
 
 function App() {
+  const [isUserFromVisible, setIsUserFromVisible] = useState(false);
   return (
     <>
       <div className="card">
         <CounterButton />
         <Paragraph paragraphText="Click left button to decrease / right to increase" />
+        <Button
+          text="Show / Hide User From"
+          onClick={() => setIsUserFromVisible((prevState) => !prevState)}
+        />
       </div>
+      {isUserFromVisible && <form> User Form</form>}
     </>
   );
 }
