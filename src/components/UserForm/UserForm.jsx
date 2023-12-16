bb6cf5ebaec22c93d28ec47cc3d7f15d378931e9
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-12-16 20:22:37 +0100

    ps3-zad5

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -3,6 +3,13 @@ import "./UserForm.css";
 
 const UserForm = () => {
   const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <form className={"UserForm"}>
       User form
@@ -12,6 +19,15 @@ const UserForm = () => {
         value={userName}
         onChange={(e) => setUserName(e.target.value)}
       />
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="button" onClick={togglePasswordVisibility}>
+        {showPassword ? "Hide Password" : "Show Password"}
+      </button>
     </form>
   );
 };
