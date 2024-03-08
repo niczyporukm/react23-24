8209085da6f2b99d7301ceef4f8f568dc2b87ce2
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2024-03-08 18:11:01 +0100

    ps6-zad1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
+import "./index.css";
+
+const RedirectToDashboard = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/dashboard");
+  }, [navigate]);
+
+  return null;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RedirectToDashboard />,
+  },
+  {
+    path: "/dashboard",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
-    <App />
-  </>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
