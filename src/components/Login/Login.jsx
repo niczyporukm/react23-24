133c136b3a64e6cbeb94c68739639fb228c294cf
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2024-03-08 18:23:26 +0100

    ps6-zad4

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const user = {
+      username: username,
+      password: password,
+    };
+    // Here you can add authentication logic
+    console.log("user", user);
     navigate("/dashboard");
   };
 
-  return <button onClick={handleLogin}>Zaloguj</button>;
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Zaloguj</button>
+    </form>
+  );
 };
 
 export default Login;
